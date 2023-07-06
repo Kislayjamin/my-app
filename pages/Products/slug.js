1407c43d16916e744c0c238118e7a20a37eae036
Kislay <Kislay@gmail.com>
2023-07-06 08:45:45 -0700

    initial commit

---
+++ pages/Products/slug.js
@@ -0,0 +1,15 @@
+import { useRouter} from "next/router";
+
+const post = () => {
+   // const router = useRouter()
+   // const { slug } = router.query
+
+    // return <a>Post: {pid}</a>
+    return (
+        <div>
+            Slug
+        </div>
+    )
+}
+
+export default Post
